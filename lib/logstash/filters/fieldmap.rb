1f5636e72902cb0d79a10053d9f049ed2726dc42
Adam Phillabaum <adamp@payscale.com>
2016-05-26 21:30:44 -0700

    CORE-2486 - Adding support for quoted strings to be handled as a single field

--- lib/logstash/filters/fieldmap.rb
+++ lib/logstash/filters/fieldmap.rb
@@ -4,10 +4,10 @@ require "logstash/namespace"
 
 # This filter will split the src_field on delimiter and then
 # create a map in the dst_field by pairing the elements of
-# the keys config item with the values from the split src 
+# the keys config item with the values from the split src
 # field.
 #
-# If the number of elements in the split src_field and the 
+# If the number of elements in the split src_field and the
 # supplied keys is not the same the event will receive a tag
 # and be sent along
 #
@@ -26,7 +26,7 @@ class LogStash::Filters::FieldMap < LogStash::Filters::Base
   # }
   #
   config_name "fieldmap"
-  
+
   # Field to use as source for map values
   config :src_field, :validate => :string, :default => "message"
 
@@ -37,14 +37,18 @@ class LogStash::Filters::FieldMap < LogStash::Filters::Base
   # Regex to split src_field by
   config :regex, :validate => :string, :default => '[[:space:]]'
 
+  # Regroup things that were unwantedly split by the regex
+  config :text_qualifier, :validate => :string, :default => false
+
   # List of keys to use in the dst map
   config :keys, :validate => :array, :required => true
-  
+
   # Append value to the tag field when the mapping failed
   config :map_failure, :validate => :string, :default => "_fieldmapfailed"
+
   public
   def register
-    # Add instance variables 
+    # Add instance variables
   end # def register
 
   public
@@ -56,10 +60,53 @@ class LogStash::Filters::FieldMap < LogStash::Filters::Base
       #  the key lenght, if not, explode
       split_src = event[@src_field].split(/#{@regex}/)
       @logger.debug? and @logger.debug("split_src is: ", :split_src => split_src)
+
+      if @text_qualifier
+        split_src_new_idx = split_src_orig_idx = 0
+        qualifier_start_idx = -1
+        split_src.map do |val|
+
+          val = val.strip   # this will cause problems if the text_qualifier is a space character
+
+          # Handle the case where we're in a qualified string
+          if val.start_with? @text_qualifier
+            qualifier_start_idx = split_src_orig_idx
+          elsif val.end_with? @text_qualifier
+            # TODO make this put in whatever it found to split on... not just a space
+            val = split_src[qualifier_start_idx..split_src_orig_idx].join(' ')
+            val = val.tr(@text_qualifier, "") # strips off the qualifier marks
+            qualifier_start_idx = -1
+          end
+
+          # this is the "not in the middle of a qualified string" (a.k.a normal) case
+          if qualifier_start_idx < 0
+            split_src[split_src_new_idx] = val
+            split_src_new_idx += 1
+          end
+
+          split_src_orig_idx += 1
+
+          # Handle the case where there is no "end" to the qualified string
+          if split_src_orig_idx >= split_src.length
+            event["tags"] ||= []
+            event["tags"] << @map_failure unless event["tags"].include?(@map_failure)
+            @logger.info? and @logger.info("Event failed field map")
+          end
+        end
+
+        # cleanup the extra from the end now
+        split_src_new_idx -= 1
+        split_src_orig_idx -= 1
+        while split_src_orig_idx > split_src_new_idx do
+          split_src.delete_at(split_src_orig_idx)
+          split_src_orig_idx -= 1
+        end
+      end
+
       if split_src.length == @keys.length
         event[@dst_field] = {}  #  don't need to save off the source data, already split into split_src
         idx = 0
-        split_src.map do |val| 
+        split_src.map do |val|
           val = val.strip
           if val.include?('{')
             begin
@@ -67,7 +114,7 @@ class LogStash::Filters::FieldMap < LogStash::Filters::Base
             rescue LogStash::Json::ParserError  # if its not valid json leave it alone
             end
           end
-          event[@dst_field][@keys[idx]] = val 
+          event[@dst_field][@keys[idx]] = val
           idx=idx+1
         end
         filter_matched(event)
--- spec/filters/fieldmap_spec.rb
+++ spec/filters/fieldmap_spec.rb
@@ -43,4 +43,62 @@ describe LogStash::Filters::FieldMap do
       expect(subject['mapped_message']).to eq({'Greeting' => 'Hello', 'Target' => 'Haters'})
     end
   end
+
+  describe "should error on field to value mismatch" do
+    let(:config) do <<-CONFIG
+      filter {
+        fieldmap {
+          src_field => 'message'
+          dst_field => 'mapped_message'
+          keys => ['timestamp', 'log']
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => '"2016-05-26 05:00:00PST" jibberish') do
+      expect(subject).to include("message")
+      expect(subject['tags']).to eq(["_fieldmapfailed"])
+    end
+  end
+
+  describe "should successfully allow \" to quote strings" do
+    let(:config) do <<-CONFIG
+      filter {
+        fieldmap {
+          src_field => 'message'
+          dst_field => 'mapped_message'
+          text_qualifier => '"'
+          keys => ['timestamp', 'log']
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => '"2016-05-26 05:00:00PST" jibberish') do
+      expect(subject).to include("message")
+      expect(subject['mapped_message']).to eq({'timestamp' => '2016-05-26 05:00:00PST', 'log' => 'jibberish'})
+    end
+  end
+
+  describe "should successfully allow qualification on several strings" do
+    let(:config) do <<-CONFIG
+      filter {
+        fieldmap {
+          src_field => 'message'
+          dst_field => 'mapped_message'
+          text_qualifier => '"'
+          keys => ['timestamp', 'log', 'log2']
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => '"2016-05-26 05:00:00PST this is more words" jibberish "ron swanson"') do
+      expect(subject).to include("message")
+      expect(subject['mapped_message']).to eq({'timestamp' => '2016-05-26 05:00:00PST this is more words', 'log' => 'jibberish', 'log2' => 'ron swanson'})
+    end
+  end
+
+
 end
